47a1244aa67fc2081a9ecc74f879f08c86d281b2
Lorel <lorel.oboue@gmail.com>
2022-07-04 18:03:01 +0100

    working on state, rendering and displaying pokemon cards

--- src/index.js
+++ src/index.js
@@ -1,22 +1,70 @@
 const pokeForm = document.querySelector(".poke-form");
+// targeting the input form and submit
 const pokeList = document.querySelector(".poke-list");
+// targeting the pokemon ul list
+
+const state = {
+  pokemons: [],
+};
+
+const setCurrentState = (pokemons) => {
+  state.pokemons = pokemons;
+};
+
+const renderCurrentState = () => {
+  pokemonsUl.innerHTML = "";
+
+  state.pokemons.forEach((todo) => {
+    const todoLi = document.createElement("li");
+    pokemonsUl.append(todoLi);
+
+    if (todo.completed) {
+      todoLi.setAttribute("class", "completed");
+    }
+
+    todoLi.innerText = todo.title;
+  });
+};
+
+const get = () => {
+  fetch("http://localhost:3000/pokemons")
+    .then(function (Response) {
+      console.log("the response", Response);
+      return Response.json();
+    })
+    .then(function (pokemons) {
+      console.log("the data", pokemons);
+      setCurrentState(pokemons);
+      renderCurrentState();
+    });
+};
+
+get();
 
 function addPokemon(pokemon) {
   const liEl = document.createElement("li");
+  //creates the pokemon card li
   const imgEl = document.createElement("img");
+  // creates the card img
   const h2El = document.createElement("h2");
+  // creates the pokemon card title
 
   liEl.classList.add("pokemon");
+  //creates a class
   imgEl.src = pokemon.image;
 
   h2El.innerText = pokemon.name;
+  //sets the inner text of h2 to the pokemons name
 
   liEl.append(imgEl, h2El);
+  //appends the image and title of the pokemon to the li
   pokeList.append(liEl);
+  // appends the li to the ul
 }
 
 function addPokemons(pokemons) {
-  pokemons.forEach(pokemon => addPokemon(pokemon))
+  pokemons.forEach((pokemon) => addPokemon(pokemon));
+  //for each loop that adds all pokemons from the data
 }
 
 function listenToAddPokemonForm() {
@@ -24,7 +72,7 @@ function listenToAddPokemonForm() {
     event.preventDefault();
     const pokemon = {
       name: pokeForm.name.value,
-      image: pokeForm.image.value
+      image: pokeForm.image.value,
     };
 
     // CREATE
@@ -47,9 +95,9 @@ function init() {
   listenToAddPokemonForm();
 
   // READ
-  // fetch("http://localhost:3000/pokemons")
-  //   .then(res => res.json());
-  //   .then(pokemons => addPokemons(pokemons));
+  fetch("http://localhost:3000/pokemons")
+    .then((res) => res.json())
+    .then((pokemons) => addPokemons(pokemons));
 }
 
 init();
